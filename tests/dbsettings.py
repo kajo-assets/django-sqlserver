edda8119c67b4366fa7df24e3a04110f71a0d540
Mikhail Denisenko <denisenkom@gmail.com>
2013-03-17 13:25:39 -0400

    remove default sqlinstance

--- tests/dbsettings.py
+++ tests/dbsettings.py
@@ -5,7 +5,7 @@ import os
 
 # use old style settings for non-django dbapi tests
 DATABASE_NAME = 'django_test_backend'
-DATABASE_HOST = os.environ['COMPUTERNAME'] + '\\' + os.environ.get('SQLINSTANCE', 'ss2008')
+DATABASE_HOST = os.environ['COMPUTERNAME'] + '\\' + os.environ.get('SQLINSTANCE', '')
 DATABASE_USER = ''
 DATABASE_PASSWORD = ''
 DATABASE_COMMAND_TIMEOUT = 30
