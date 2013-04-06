3847543781e623db39622093d27440b54bc278d4
Mikhail Denisenko <denisenkom@gmail.com>
2013-04-06 01:46:58 -0400

    bugfix

--- sqlserver/pymssql/introspection.py
+++ sqlserver/pymssql/introspection.py
@@ -4,8 +4,8 @@ import pymssql
 class DatabaseIntrospection(BaseSqlDatabaseIntrospection):
     data_types_reverse = {
         #'AUTO_FIELD_MARKER': 'AutoField',
-        pymssql.STRING: 'CharField',
-        pymssql.NUMBER: 'IntegerField',
-        pymssql.DECIMAL: 'DecimalField',
-        pymssql.DATETIME: 'DateTimeField',
+        #pymssql.STRING: 'CharField',
+        #pymssql.NUMBER: 'IntegerField',
+        #pymssql.DECIMAL: 'DecimalField',
+        #pymssql.DATETIME: 'DateTimeField',
     }
