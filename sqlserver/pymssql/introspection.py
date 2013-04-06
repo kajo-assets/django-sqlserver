27473706ea440b93005394aad803b76acba9aa6d
Mikhail Denisenko <denisenkom@gmail.com>
2013-04-06 01:40:21 -0400

    bugfix

--- sqlserver/pymssql/introspection.py
+++ sqlserver/pymssql/introspection.py
@@ -1,23 +1,23 @@
 from ..introspection import BaseSqlDatabaseIntrospection
-import pytds
+import pymssql
 
 class DatabaseIntrospection(BaseSqlDatabaseIntrospection):
     data_types_reverse = {
         #'AUTO_FIELD_MARKER': 'AutoField',
-        pytds.SYBBIT: 'BooleanField',
-        pytds.XSYBCHAR: 'CharField',
-        pytds.XSYBNCHAR: 'CharField',
-        pytds.SYBDECIMAL: 'DecimalField',
-        pytds.SYBNUMERIC: 'DecimalField',
-        #pytds.adDBTimeStamp: 'DateTimeField',
-        pytds.SYBREAL: 'FloatField',
-        pytds.SYBFLT8: 'FloatField',
-        pytds.SYBINT4: 'IntegerField',
-        pytds.SYBINT8: 'BigIntegerField',
-        pytds.SYBINT2: 'IntegerField',
-        pytds.SYBINT1: 'IntegerField',
-        pytds.XSYBVARCHAR: 'CharField',
-        pytds.XSYBNVARCHAR: 'CharField',
-        pytds.SYBTEXT: 'TextField',
-        pytds.SYBNTEXT: 'TextField',
+        pymssql.SYBBIT: 'BooleanField',
+        pymssql.XSYBCHAR: 'CharField',
+        pymssql.XSYBNCHAR: 'CharField',
+        pymssql.SYBDECIMAL: 'DecimalField',
+        pymssql.SYBNUMERIC: 'DecimalField',
+        #pymssql.adDBTimeStamp: 'DateTimeField',
+        pymssql.SYBREAL: 'FloatField',
+        pymssql.SYBFLT8: 'FloatField',
+        pymssql.SYBINT4: 'IntegerField',
+        pymssql.SYBINT8: 'BigIntegerField',
+        pymssql.SYBINT2: 'IntegerField',
+        pymssql.SYBINT1: 'IntegerField',
+        pymssql.XSYBVARCHAR: 'CharField',
+        pymssql.XSYBNVARCHAR: 'CharField',
+        pymssql.SYBTEXT: 'TextField',
+        pymssql.SYBNTEXT: 'TextField',
     }
