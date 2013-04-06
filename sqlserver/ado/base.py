2421db8b5871002913291b6904be558b12d82937
Mikhail Denisenko <denisenkom@gmail.com>
2013-04-06 18:44:03 -0400

    fixed exception thrown from ado backend

--- sqlserver/ado/base.py
+++ sqlserver/ado/base.py
@@ -1,6 +1,7 @@
-"""Microsoft SQL Server database backend for Django."""
-
+import sys
+from django.db import utils
 from . import dbapi as Database
+adodb = Database
 
 from sqlserver.base import (
     SqlServerBaseWrapper,
@@ -33,10 +34,48 @@ class DatabaseWrapper(SqlServerBaseWrapper):
     def _cursor(self):
         if self.connection is None:
             self.__connect()
-        return Database.Cursor(self.connection)
+        return CursorWrapper(Database.Cursor(self.connection))
 
     def _is_sql2005_and_up(self, conn):
         return int(conn.adoConnProperties.get('DBMS Version').split('.')[0]) >= 9
 
     def _is_sql2008_and_up(self, conn):
         return int(conn.adoConnProperties.get('DBMS Version').split('.')[0]) >= 10
+
+
+class CursorWrapper(object):
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.cursor.close()
+
+    def execute(self, sql, params = ()):
+        try:
+            return self.cursor.execute(sql, params)
+        except adodb.IntegrityError, e:
+            raise utils.IntegrityError, utils.IntegrityError(*tuple(e)), sys.exc_info()[2]
+        except adodb.DatabaseError, e:
+            raise utils.DatabaseError, utils.DatabaseError(*tuple(e)), sys.exc_info()[2]
+        except adodb.Error:
+            raise
+
+    def executemany(self, sql, params):
+        try:
+            return self.cursor.executemany(sql, params)
+        except adodb.IntegrityError, e:
+            raise utils.IntegrityError, utils.IntegrityError(*tuple(e)), sys.exc_info()[2]
+        except adodb.DatabaseError, e:
+            raise utils.DatabaseError, utils.DatabaseError(*tuple(e)), sys.exc_info()[2]
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        else:
+            return getattr(self.cursor, attr)
+
+    def __iter__(self):
+        return iter(self.cursor)
