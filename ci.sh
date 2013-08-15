03006d804d5a8de94270227f0f361323a601464b
Mikhail Denisenko <denisenkom@gmail.com>
2013-08-14 23:23:26 -0400

    added ci.sh script for linux

---
+++ ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+virtualenv --no-site-packages env
+. env/bin/activate
+pip install django==$DJANGO_VER --use-mirrors
+
+if [ $BACKEND = sqlserver.pytds ]; then
+    pip install -e git+git://github.com/denisenkom/pytds.git#egg=pytds
+fi
+if [ $BACKEND = sqlserver.pymssql ]; then
+    pip install cython hg+https://denisenkom@code.google.com/r/denisenkom-pymssql/ --use-mirrors
+fi
+cat > tests/local_settings.py << EOF
+DATABASE_USER = '$SQLUSER'
+DATABASE_PASSWORD = '$SQLPASSWORD'
+EOF
+export COMPUTERNAME=$HOST
+python tests/test_main/manage.py test --noinput
+python tests/test_regex_compare/manage.py test --noinput
+python tests/test_inspectdb/manage.py test --noinput
+python tests/test_stardardapps/manage.py test --noinput
