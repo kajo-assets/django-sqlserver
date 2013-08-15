44e110d9831fe58cc576ced16c58ab1798900273
Mikhail Denisenko <denisenkom@gmail.com>
2013-08-14 23:36:52 -0400

    improvements for ci.sh

--- ci.sh
+++ ci.sh
@@ -5,10 +5,10 @@ $PYTHONHOME/bin/virtualenv --no-site-packages env
 python env/bin/pip install django==$DJANGO_VER --use-mirrors
 
 if [ $BACKEND = sqlserver.pytds ]; then
-    pip install -e git+git://github.com/denisenkom/pytds.git#egg=pytds
+    python env/bin/pip install -e git+git://github.com/denisenkom/pytds.git#egg=pytds
 fi
 if [ $BACKEND = sqlserver.pymssql ]; then
-    pip install cython hg+https://denisenkom@code.google.com/r/denisenkom-pymssql/ --use-mirrors
+    python env/bin/pip install cython hg+https://denisenkom@code.google.com/r/denisenkom-pymssql/ --use-mirrors
 fi
 cat > tests/local_settings.py << EOF
 DATABASE_USER = '$SQLUSER'
