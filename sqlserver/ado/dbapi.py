db5e44dfc9a78a1260cca9d411f5d5c20f963b1b
Mikhail Denisenko <denisenkom@gmail.com>
2013-08-25 12:30:29 -0400

    fixed django test: test_raw_sql (modeltests.timezones.tests.NewDatabaseTests), support timezone for query parameters.

--- sqlserver/ado/dbapi.py
+++ sqlserver/ado/dbapi.py
@@ -215,7 +215,13 @@ def _configure_parameter(p, value):
             p.Precision = digit_count + exponent
 
     elif isinstance(value, datetime.time):
+            if getattr(settings, 'USE_TZ', False) and value.tzinfo:
+                value = value.astimezone(timezone.utc)
             p.Value = datetime.datetime(1,1,1, value.hour, value.minute, value.second)
+    elif isinstance(value, datetime.datetime):
+            if getattr(settings, 'USE_TZ', False) and value.tzinfo:
+                value = value.astimezone(timezone.utc)
+            p.Value = value
     else:
         # For any other type, set the value and let pythoncom do the right thing.
         p.Value = value
