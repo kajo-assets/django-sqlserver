5df87602d009654890e05f1a9ee1445d9b15724a
Mikhail Denisenko <denisenkom@gmail.com>
2013-09-06 22:12:44 -0400

    added sqls for deleting index and renaming table

--- sqlserver/schema.py
+++ sqlserver/schema.py
@@ -6,3 +6,5 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     sql_delete_table = "DROP TABLE %(table)s"
     sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s"
     sql_create_column = "ALTER TABLE %(table)s ADD %(column)s %(definition)s"
+    sql_delete_index = "DROP INDEX %(name)s ON %(table)s"
+    sql_rename_table = "sp_rename %(old_table)s, %(new_table)s"
